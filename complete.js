96235a2291e3b4721f77cd7e70171d18a480b9c1
yEAh-Games <tallinnquinton@gmail.com>
2023-06-12 00:19:53 -0400

    More updates

--- complete.js
+++ complete.js
@@ -1,22 +1,33 @@
-$(document).ready(function() {
-    function redirect() {
-      $("body").addClass("screen-shake");
-            setTimeout(function() {
-        $("body").addClass("transition");
-        
-        setTimeout(function() {
-          window.location.href = "https://example.com"; 
-        }, 1000); 
-      }, 1000);
-    }
-  
-    function checkForComplete() {
-      if ($(".complete").length > 0) {
-        setTimeout(redirect, 1000); 
+$(document).ready(function () {
+  function redirect() {
+    $("body").addClass("screen-shake");
+    setTimeout(function () {
+      $("body").addClass("transition");
+
+      var anchor = window.location.hash.substr(1);
+
+      var redirectUrls = {
+        "1": "/writing/1",
+        "2": "/reading/1",
+        "3": "/writing/2",
+        "4": "/reading/2",
+      };
+
+      if (redirectUrls.hasOwnProperty(anchor)) {
+        window.location.href = redirectUrls[anchor];
       } else {
-        setTimeout(checkForComplete, 50); 
+        window.location.href = "https://example.com";
       }
+    }, 1000);
+  }
+
+  function checkForComplete() {
+    if ($(".complete").length > 0) {
+      setTimeout(redirect, 1000);
+    } else {
+      setTimeout(checkForComplete, 50);
     }
-  
-    checkForComplete(); 
-  });
+  }
+
+  checkForComplete();
+});
